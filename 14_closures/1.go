232f4dc728c46de154d61def69077e9d8d22dbb0
Aman Singh3 <int1913@ggn-mac-int1913.mmt.com>
2025-11-19 12:17:36 +0530

    Done with closures

--- 14_closures/1.go
+++ 14_closures/1.go
@@ -2,11 +2,35 @@ package main
 
 import "fmt"
 
-func counter() func() int{
-	cnt := 0;
-	
+func counter() func() int {
+	cnt := 0
+
+	return func() int {
+		cnt++
+		return cnt //this variable will be stored in the closure environment -> heap
+	}
 }
 
-func main(){
+func multiplier(factor int) func(int) int {
+	return func(n int) int {
+		return n * factor
+	}
+}
+
+func main() {
+	//eg1
+	increment := counter()
+
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+
+	//eg 2
+	double := multiplier(2)
+	triple := multiplier(3)
+
+	fmt.Println(double(5))
+	fmt.Println(triple(5))
 
 }
